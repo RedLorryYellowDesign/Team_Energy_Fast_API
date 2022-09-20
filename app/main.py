7f6eab0ef5de4de72380e57d102915219a3e73a7
Jordan Harris <jordan.lee.harris@icloud.com>
2022-09-20 19:43:25 +0100

    Merged current Branches

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ def check():
 @app.get("/model/predict")
 async def predict_Model(name, tariff):
     # Joblib import model
-    filename = f'Team_Energy/Prophet_models/model_{name}_{tariff}.joblib'
+    filename = f'app/Team_Energy/Prophet_models/model_{name}_{tariff}.joblib'
     m = joblib.load(filename)
     train_df, test_df,val_df = create_data(name, tariff)
     train_df, test_df = create_data(name, tariff)
@@ -39,10 +39,29 @@ async def predict_Model(name, tariff):
     forecast_list = forecast.tolist()
     return {'prediction': [forecast_list], 'accuracy': mape }
 
+
+
+
+# ef forecast_model(m,train_wd,test_wd,add_weather=True):
+#     future = m.make_future_dataframe(periods=48*27+1, freq='30T')
+#     if add_weather==True:
+#         wd_filt_future=future[['ds']].merge(pd.concat([future,pd.concat([train_wd,test_wd],axis=0)]),left_on='ds',right_on='DateTime',how='inner').drop(columns='DateTime')
+#         temp_future=wd_filt_future['temperature'].interpolate(method='linear')
+#         future['temp']=temp_future
+#         fcst = m.predict(future)
+#     else:
+#         fcst = m.predict(future)
+#     forecast=fcst.loc[fcst['ds']>='2014-02-01 00:00:00',['ds','yhat']]
+#     return forecast
+
+# Evaluate model
+def evaluate(actual,forecast):
+    return np.round(mean_absolute_percentage_error(actual,forecast),4)
+
 @app.get("/model/RNN_predict")
 async def RNN_Model(name, tariff):
     # Joblib import model
-    filename = f'Team_Energy/RNN/RNNmodel_{name}_{tariff}.joblib'
+    filename = f'app/Team_Energy/RNN/RNNmodel_{name}_{tariff}.joblib'
     m = joblib.load(filename)
     train_df, test_df,val_df = create_data(name, tariff)
     X_train, y_train, X_test, sc, test_set = prepare_sequences(train_df, test_df,val_df)
